ee6c8a22250da5aef8be4fc70aabf58bb3601834
Michael Gonzalez <michaelgonzalez0723@gmail.com>
2022-06-07 16:27:13 -0400

    commit tree 1ec4ece66cac941aba7d88c99bcc838a2f21657e

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Write your code here!
+const main =document.querySelector('#main') .remove()
+
+const newHeader =document.createElement('h1')
+newHeader.id='victory'
+newHeader.innerHTML="Michael is the champion"
+main.appendChild(newHeader)
+//console.log(victory)
